6fe81a76719557d25fc947e582dbfa77618d1eee
Anestis Gkanogiannis <ganoyan@gmail.com>
2025-05-19 13:16:57 +0400

    Update galaxy test action

--- fastreer-galaxy/galaxy_test_tools.sh
+++ fastreer-galaxy/galaxy_test_tools.sh
@@ -6,6 +6,6 @@ tools_root="$1"
 xml_files=$(find ${tools_root} -name '*.xml' -print0 | tr '\0' ' ')
 planemo \
     test --skip_venv \
-    --test_output fastreer-galaxy-test-report.html \
+    --test_output ${tools_root}/galaxy-test-report.html \
     --test_output_json /dev/null \
     ${xml_files}
