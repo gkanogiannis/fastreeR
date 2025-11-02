23d3c2db2bc7876574c11df5434cde131db991de
Anestis Gkanogiannis <ganoyan@gmail.com>
2025-11-02 02:33:27 +0100

    Update galaxy_update_versions.sh

--- deploy/fastreer-galaxy/galaxy_update_versions.sh
+++ deploy/fastreer-galaxy/galaxy_update_versions.sh
@@ -7,15 +7,9 @@ VERSION="$2"
 echo "Updating Galaxy tool XMLs to fastreer version: ${VERSION}"
 
 # Update version in <tool ... version="...">
-find ${tools_root}/tools -name "*.xml" -print0 | while IFS= read -r -d '' file; do
+find ${tools_root}/tools -name ".xml" -print0 | while IFS= read -r -d '' file; do
     echo "Updating tool version in: $file"
-    sed -i -E "s/(<tool[^>]*version=\")([^\"]+)(\"[^>]*>)/\1${VERSION}\3/" "$file"
-done
-
-# Update fastreer package requirement version
-find ${tools_root}/tools -name "*.xml" -print0 | while IFS= read -r -d '' file; do
-    echo "Updating Conda requirement in: $file"
-    sed -i -E "s|(<requirement[^>]*>fastreer)([^<]*)(</requirement>)|\1=${VERSION}\3|g" "$file"
+    sed -i -E "s|(<token name=\"@TOOL_VERSION@\">)[^<]+(</token>)|\1${VERSION}\2|" "$file"
 done
 
 echo "All Galaxy tool versions updated to ${VERSION}"
